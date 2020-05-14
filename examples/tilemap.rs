156f073a3e0ffbf479437d3d69f74f8a7ede30bb
erinpentecost <erin@pentecost.email>
2020-05-13 19:34:40 -0700

    revising tilemap example

--- examples/tilemap.rs
+++ examples/tilemap.rs
@@ -6,16 +6,18 @@ pub fn main() {
     // and allow for any one of them to be unknown.
     let mut m = markovr::MarkovChain::<char>::new(4, &[0, 1, 2, 3]);
 
-    let train: Vec<Vec<char>> = "                                
-┏━━━━┳━━━━━━┓ ┏━┳━━┳━━━━━━━━━━┓ 
-┃    ┃ ┏━┓  ┃ ┃ ┃  ┃          ┃ 
-┣━━━━╋━╋━╋━━╋━┫ ┃ ┏╋━━━━┓     ┃ 
-┃    ┃ ┗━┛  ┃ ┃ ┃ ┗╋━━━━┛     ┃ 
-┗━━━━┻━━━━━━┛ ┗━┻━━┻━━━━━━━━━━┛ 
-                                
+    let train: Vec<Vec<char>> = "           
+ ┏━━━┓     
+ ┃   ┃     
+ ┃   ┣━━━┓ 
+ ┃   ┃   ┃ 
+ ┃   ┣━━━┛ 
+ ┃   ┃     
+ ┗━━━┛     
+           
 "
     .lines()
-    .map(|c| c.chars().take(32).collect())
+    .map(|c| c.chars().take(12).collect())
     .collect();
 
     // Train the model.
@@ -34,17 +36,41 @@ pub fn main() {
     }
 
     // Generate values from the model.
-    let mut map: [[Option<char>; 16]; 16] = [[None; 16]; 16];
-    //let mut rand_map : Vec<Vec<char>> = vec!(vec!(&['┏', '━']),vec!(&['┃']));
-    for r in 1..15 {
-        for c in 1..15 {
-            let neighbors = &[map[r - 1][c], map[r][c - 1], map[r][c + 1], map[r + 1][c]];
+    const DIM: usize = 16;
+    let mut map: [[Option<char>; DIM]; DIM];
+    'gen: loop {
+        map = [[None; DIM]; DIM];
+        // Fill in spaces around the border. This isn't necessary,
+        // but should prevent dangling lines in the output.
+        for i in 0..DIM {
+            map[i][0] = Some(' ');
+            map[i][DIM - 1] = Some(' ');
+            map[0][i] = Some(' ');
+            map[DIM - 1][i] = Some(' ');
+        }
+        // Iterate on all non-None spaces and fill them in.
+        for r in 1..(DIM - 1) {
+            for c in 1..(DIM - 1) {
+                let neighbors = &[map[r - 1][c], map[r][c - 1], map[r][c + 1], map[r + 1][c]];
 
-            map[r][c] = m.generate_from_partial(neighbors);
+                map[r][c] = m.generate_from_partial(neighbors);
+                match map[r][c] {
+                    Some(_) => {}
+                    // We saw a case that wasn't in our training data,
+                    // so throw it away and try again.
+                    None => {
+                        continue 'gen;
+                    }
+                }
+            }
+        }
+        break 'gen;
+    }
+
+    for r in 1..(DIM - 1) {
+        for c in 1..(DIM - 1) {
             match map[r][c] {
-                Some(c) => print!("{}", c),
-                // We saw a case that wasn't in our training data,
-                // so print a placeholder.
+                Some(v) => print!("{}", v),
                 None => print!("?"),
             }
         }
@@ -52,19 +78,19 @@ pub fn main() {
     }
     // Prints:
     /*
-    ━━━━━━━━━┓  ┃
-             ┃  ┗━
-          ┏━━╋━━━━
-    ━━┓   ┗━━┛
-      ┃   ?━┓?━━━━
-    ━━╋━━━━━┛
-      ┗━━━━━━━┓
-     ┏?━━━━━━━┛  ┏
-    ━╋━┓      ?━━╋
-     ┗━╋━━━━┳━╋━━┻
-    ━━━┻━━┓ ┃ ┃  ?
-       ?━━┛ ┃ ┃
-       ┏━┓? ┃ ┃ ┏━
-     ┏━╋━┛  ┃ ┃ ┗━
-        */
+           ┏━━━━┓
+           ┃    ┃
+           ┃    ┃
+      ┏━━━━┛    ┃
+      ┃         ┃
+    ┏━┛         ┃
+    ┃           ┃
+    ┃           ┃
+    ┃           ┃
+    ┣━━━━━━━━━━━┛
+    ┃
+    ┣━━━┓
+    ┃   ┃
+    ┗━━━┛
+       */
 }
