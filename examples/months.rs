f701c81a1779e80d51f2aad40c9749af42dd66d9
erin <erin@pentecost.email>
2020-10-16 22:10:49 -0700

    more useful example

--- examples/months.rs
+++ examples/months.rs
@@ -1,8 +1,8 @@
 extern crate markovr;
 
 pub fn main() {
-    // Create a new, first-order Markov Chain for generating month names.
-    let mut m = markovr::MarkovChain::<char>::new(1, &[0]);
+    // Create a new, second-order Markov Chain for generating month names.
+    let mut m = markovr::MarkovChain::<char>::new(2, &[0,1]);
 
     let train: Vec<Vec<char>> = "january
 february
@@ -17,63 +17,9 @@ october
 november
 december
 nisan
-iyar
-sivan
-tammuz
-av
-elul
-tishri
-marcheshvan
-kislev
-tevet
-shevat
-adar
-muharram
-safar
-rajab
-shaban
-ramadan
-shawwal
-caitra
-vaikasi
-jyestha
-ashada
-sravana
-bhadrapada
-asvina
-kartika
-maargazhi
-pausa
-magha
-chet
-vaisakh
-jeth
-harth
-sawan
-bhadon
-assu
-katak
-maghar
-poh
-magh
-phagun
-gormanuour
-ylir
-morsugur
-porri
-goa
-einmanuour
-harpa
-skerpla
-solmanuour
-heyannir
-tvimanuour
-haustmanuour
 thout
 paopi
 hathor
-koiak
-tooba
 emshir
 paremhat
 paremoude
@@ -81,11 +27,8 @@ pashons
 paoni
 epip
 mesori
-vendemiarie
 brumaire
 frimaire
-nivose
-pluviose
 ventose
 germinal
 floreal
@@ -101,36 +44,43 @@ fructidor
     // Train the model.
     for r in 0..(train.len() - 1) {
         let ref row = train[r];
-        for c in 1..(row.len() - 1) {
-            m.train(&[row[c - 1]], train[r][c], 1);
+        // Handle first characters.
+        // $ is used as a beginning-of-word indicator.
+        // % is also needed for that indicator since
+        // the Markov chain is second-order.
+        m.train(&['$', '%'], row[0], 1);
+        m.train(&['%', row[0]], row[1], 1);
+        // Handle middle characters
+        for c in 2..(row.len() - 1) {
+            m.train(&[row[c - 2], row[c - 1]], train[r][c], 1);
         }
-        // Cap the end
-        m.train(&[row[row.len()-1]], ' ', 1);
+        // Cap the end.
+        m.train(&[row[row.len()-2], row[row.len()-1]], ' ', 1);
     }
 
     // Generate values from the model.
-    'gen: loop {
-        let mut month = Vec::new();
-        let mut last: Option<char> = m.generate_from_partial(&[None]);
-        while let Some(c) = last{
-            // Did we end?
-            if c == ' '{
-                break;
+    for _ in 0..12 {
+        'gen: loop {
+            let mut month = Vec::new();
+            let mut lastlast = Some('%');
+            let mut last: Option<char> = m.generate_from_partial(&[Some('$'), Some('%')]);
+            while let Some(c) = last{
+                // Did we end?
+                if c == ' '{
+                    break;
+                }
+                else {
+                    // Tack on the character
+                    month.push(c);
+                    // Find the next character
+                    let window: [char; 2] = [lastlast.unwrap(), last.unwrap()];
+                    lastlast = last;
+                    last = m.generate(&window);
+                }
             }
-            else {
-                // Tack on the character
-                month.push(c);
-                // Find the next character
-                last = m.generate(&[last.unwrap()]);
-            }
+            let month_name: String = month.into_iter().collect();
+            print!("{}\n", month_name);
+            break 'gen;
         }
-        // Throw away items that are too short
-        if month.len() < 4 {
-            month.clear();
-            continue 'gen;
-        }
-        let month_name: String = month.into_iter().collect();
-        print!("{}\n", month_name);
-        break 'gen;
     }
 }
